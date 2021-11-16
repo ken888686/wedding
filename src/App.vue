e9c6a01b3514b8bff68e6520e7f296c8af962418
Aaron <ken888686@gmail.com>
2021-11-16 16:53:42 +0800

    add Buttons

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,16 @@
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import Header from "./components/Header.vue";
 import Banner from "./components/Banner.vue";
+import Buttons from "./components/Buttons.vue";
 import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <Header />
   <Banner />
+  <div class="container">
+    <Buttons />
+  </div>
   <Footer />
 </template>
 
