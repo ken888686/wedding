604f1f3daebe7d5b779114e22c5e7dedcff48045
Aaron <ken888686@gmail.com>
2021-11-16 16:42:20 +0800

    add footer

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.scss";
+import "./assets/all.scss";
+import "bootstrap";
+
+createApp(App).mount("#app");
