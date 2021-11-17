3fd731b9b02944f630bc46d8315773722005beab
ken888686@gmail.com <asas456789>
2021-11-17 17:37:28 +0800

    add node.js ci

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:ken888686/ken888686.github.io.git main
+# git push -f git@github.com:ken888686/ken888686.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
