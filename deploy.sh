fb7c5b26de969a57392e81487fa552736f726938
Aaron <ken888686@gmail.com>
2021-11-18 13:39:20 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,12 +20,11 @@ git init
 git add -A
 git commit -m 'deploy'
 
-
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:ken888686/ken888686.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f https://github.com/ken888686/ken888686.github.io.git master:gh-pages
+git push -f git@github.com:ken888686/ken888686.github.io.git main:gh-pages
 
 cd -
