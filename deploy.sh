01de9420717649de65c35f51cb8b115a1712f99e
Aaron <ken888686@gmail.com>
2021-11-18 13:36:41 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -26,6 +26,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f https://github.com/ken888686/ken888686.github.io.git main:gh-pages
+git push -f https://github.com/ken888686/ken888686.github.io.git master:gh-pages
 
 cd -
