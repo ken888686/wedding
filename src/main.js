9af5df4c521491c36a3f6938455063448f7b3161
Aaron <ken888686@gmail.com>
2021-11-26 14:16:55 +0800

    add bootstrap

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import 'bootstrap/scss/bootstrap.scss';
+import 'bootstrap-icons/font/bootstrap-icons.scss';
+import 'bootstrap';
+
 createApp(App).use(router).mount('#app');
